9fd6d30c33a1bba3d5b46ac519b1b9e4440f4690
Tenzin Chosang <tenzinchosang.dev@gmail.com>
2023-03-21 12:28:12 -0400

    level up girl

--- week6/week6day2.js
+++ week6/week6day2.js
@@ -12,3 +12,15 @@ function greet(name){
     let person = name
     return `Hello, ${person} how are you doing today?` 
   }
+
+// Bob needs a fast way to calculate the 
+// volume of a cuboid with three values: 
+// the length, width and height of the cuboid. 
+// Write a function to help Bob with this calculation.
+
+
+class Kata {
+  static getVolumeOfCuboid(length, width, height) {
+    return length * width * height
+  }
+}

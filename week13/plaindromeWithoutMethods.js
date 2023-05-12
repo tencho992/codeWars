67950a566b235fe3f8cd04a319abedba770a2fc4
Tenzin Chosang <tenzinchosang.dev@gmail.com>
2023-05-12 16:28:45 -0400

    5-12

--- week13/plaindromeWithoutMethods.js
+++ week13/plaindromeWithoutMethods.js
@@ -5,7 +5,7 @@ function reversedStr(str){
     for(let i = str.length -1; i >=0; i--){
         newStr += str[i]
     }
-    return newStr ===str ? true :false;
+    return newStr === str 
 }
 
 console.log(reversedStr('hello'))

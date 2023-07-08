a3d2941c97b8d8e226eea909271e4bee283fe9d6
Tenzin Chosang <tenzinchosang.dev@gmail.com>
2023-07-08 14:58:09 -0400

    7-9

---
+++ Post-program/validPhoneNumer.js
@@ -0,0 +1,11 @@
+//Check if parameter passed through is
+//indeed a phone number
+
+//"(123) 456-7890"  => true
+//"(1111)555 2345"  => false
+//"(098) 123 4567"  => false
+
+function validPhoneNumber(phoneNumber){
+    let formula = /^\(\d{3}\) \d{3}-\d{4}$/
+    return formula.test(phoneNumber)
+  }

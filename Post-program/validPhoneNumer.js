1c1fc3a1333bee364bba195ad2a2864e9a944d83
Tenzin Chosang <tenzinchosang.dev@gmail.com>
2023-07-12 19:22:26 -0400

    regex explanation

--- Post-program/validPhoneNumer.js
+++ Post-program/validPhoneNumer.js
@@ -14,6 +14,18 @@ function validPhoneNumber(phoneNumber){
 
 let formula = /^\(\d{3}\) \d{3}-\d{4}$/
 
+//breakdown:
+// first start the sentence / and then start the expression ^
+// everytime something is declared use the special \
+// hence being with the () in area code with \(
+// then use the same special \ to declare 3 digits ex: \d{3}
+// dont forget you have to add \ for the ) to close parenthesis: \)
+// Add a space if format calls for it. Begin id next 3 digits: /d{3}
+// this is a phone number so use a - to seperate: /d{3}-/d{4}
+// this completes the 10 digit US phone number. use $ as a closing
+// NEVER FORGET: close the start of the sentence / with a identical / at the end. 
+// /^\(\d{3}\) \d{3}-\d{4}$/
+
 let regEx = /^\(\d{3}\) \d{3}-\d{4}$/
     regEx = /^\(\d{3}\) \d{3}-\d{4}$/
     regEx = /^\(\d{3}\) \d{3}-\d{4}$/

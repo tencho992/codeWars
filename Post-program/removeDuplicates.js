9f5f4c7ea1d2eab811889965d1c45b894686a0b0
Tenzin Chosang <tenzinchosang.dev@gmail.com>
2023-08-12 13:16:44 -0400

    8-12

---
+++ Post-program/removeDuplicates.js
@@ -0,0 +1,20 @@
+// Define a function that removes duplicates from an array 
+// of non negative numbers and returns it as a result.
+
+// The order of the sequence has to stay the same.
+
+// Examples:
+
+// Input -> Output
+// [1, 1, 2] -> [1, 2]
+// [1, 2, 1, 1, 3, 2] -> [1, 2, 3]
+
+function distinct(a) {
+    let newArr = []
+    for(let i = 0; i < a.length; i++){
+      if(newArr.indexOf(a[i]) === -1){
+      newArr.push(a[i])
+      }
+    }
+    return newArr
+  }
